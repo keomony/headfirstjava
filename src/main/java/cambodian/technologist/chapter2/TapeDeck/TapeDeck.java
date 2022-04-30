2dc3702a1193a50293b269d1143b646dfc22a5a0
keomony.khun <keomony.khun@moo.com>
2022-04-30 13:02:20 +0100

    Chapter 2 page 42 TapeDeck and DVDPlayer exercises

---
+++ src/main/java/cambodian/technologist/chapter2/TapeDeck/TapeDeck.java
@@ -0,0 +1,13 @@
+package cambodian.technologist.chapter2.TapeDeck;
+
+public class TapeDeck {
+    boolean canRecord = false;
+
+    void playTape() {
+        System.out.println("tape playing");
+    }
+
+    void recordTape() {
+        System.out.println("tape recording");
+    }
+}
---
+++ src/main/java/cambodian/technologist/chapter2/TapeDeck/TapeDeckTestDrive.java
@@ -0,0 +1,14 @@
+package cambodian.technologist.chapter2.TapeDeck;
+
+public class TapeDeckTestDrive {
+    public static void main(String[] args) {
+
+        TapeDeck t = new TapeDeck();
+        t.canRecord = true;
+        t.playTape();
+
+        if(t.canRecord == true) {
+            t.recordTape();
+        }
+    }
+}
